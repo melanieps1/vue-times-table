40ba8fe558a917b62138d7ec158b479f161d6910
Melanie Stoeckle <mpstoeckle@gmail.com>
2017-10-05 16:01:21 -0400

    got it generally working

--- app/main.js
+++ app/main.js
@@ -3,32 +3,15 @@ var demo = new Vue ({
 	el: '#main',
 
 	data: {
-		rowNumber: 0,
-		columnNumber: 0
+		rowNumber: 5,
+		columnNumber: 5,
+		table: document.getElementById('timesTable')
 	},
 
 	methods: {
-
 		displayTable: function() {
-			for(var i = 1; i < rowNumber; i++) {
-
-				var tableRows = "<tr>";
-
-				for(var j = 1; j < columnNumber; j++) {
-
-					var tableColumns = "<th>";
-
-					if(j == 1 || i == 1) {
-						color_td = "#ccc";
-					}
-					else {
-						color_td = "#fff";
-					}
-
-				}
-			}
+			timesTable.style.display = 'block';
 		}
-
 	}
 
 });
